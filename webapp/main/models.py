03cded072b6b02d90856bf2df2dff00d3ed6e30a
Otu Ekanem <o.ekanem@gmail.com>
2011-10-13 13:16:45 +0200

    models and views for room management

--- webapp/main/models.py
+++ webapp/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from datetime import datetime
 # Create your models here.
+
+
+class Room(models.Model):
+    owner = models.ForeignKey(User, related_name='rooms')
+    created = models.DateTimeField(default=datetime.now)
+    lastmodified = models.DateTimeField(default=datetime.now)
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=15)
+
--- webapp/main/views.py
+++ webapp/main/views.py
@@ -1,32 +1,88 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 
 from django import forms
 from muphan.models import Photo
 from muphan.views import handle
+
+import appforms
+import models as appmodels
 
 HTTP_CREATED = 201
 
 def index(request):
     return render_to_response('index.html', RequestContext(request))
 
+def login_page(request):
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/dashboard/')
+    else:
+        return render_to_response('login.html')
+
+@login_required
+def account_profile(request):
+    """display profile information form and user can save if they wish.
+       link to dashboard.
+    """
+    return render_to_response('profile.html', RequestContext(request))
+
+@login_required
+def dashboard(request):
+    """manage rooms and attached cameras - link to review rooms pictures"""
+    return render_to_response('dashboard.html', RequestContext(request))
+
 class Upload(forms.Form):
     title = forms.CharField(max_length=150, required=False)
     photo  = forms.FileField(required=False)
 
-def upload(request, username, room_id):
+@login_required
+def rooms(request):
+    if request.method == 'POST':
+        form = appforms.RoomForm(request.POST.copy())
+        if form.is_valid():
+            new_room = appmodels.Room.objects.create(
+                owner = request.user,
+                name  = form.cleaned_data['name']
+            )
+            
+            return HttpResponseRedirect('/room/%d/setup/' % new_room.id)
+        else:
+            return HttpResponse(form.errors, status_code=403)
+    else:
+        return HttpResponseRedirect('/dashboard/')
+
+@login_required
+def room_setup(request, roomid):
+    try:
+        room = request.user.rooms.get(id=roomid)
+        context = RequestContext(request, {'room': room})
+        return render_to_response('room_setup.html', context)
+    except:
+        return HttpResponseRedirect('/dashboard/?invalid_room_access')
+
+@login_required
+def room_review(request, roomid):
+    try:
+        room = request.user.rooms.get(id=roomid)
+        context = RequestContext(request, {'room': room})
+        return render_to_response('room_review.html', context)
+    except:
+        return HttpResponseRedirect('/dashboard/?invalid_room_access')
+
+@login_required
+def room_upload(request, roomid):
     if request.method == "POST":
         form = Upload(request.POST, request.FILES)
         if form.is_valid():
-            # import pdb; pdb.set_trace()
             content_type = form.files["photo"].content_type
             photo = Photo.objects.make_photo(
                 request.user, 
                 media_type = content_type,
-                unique = '%s' % room_id,
-                description = form["title"]
+                unique = '%s' % roomid,
+                description = form.cleaned_data["title"]
             )
             file_data = request.FILES["photo"]
             handle(photo, content_type, file_data)
@@ -36,6 +92,4 @@ def upload(request, username, room_id):
         else:
             return HttpResponse(str(form.errors), status_code=403)
 
-    form = Upload()
-    return HttpResponse()
-
+    return HttpResponseRedirect('/dashboard/?invalid_room_access')
